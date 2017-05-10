fa58bd719ed60137232f6b1fa1795f11e313374a
kueikuei <kueikuei@users.noreply.github.com>
2017-05-10 15:21:01 +0800

    Add files via upload

--- JSONP.js
+++ JSONP.js
@@ -1,12 +1,44 @@
-window.onload = function(){
-	// var url = "https://raw.githubusercontent.com/bethrobson/Head-First-HTML5/master/chapter6/sales.json"; 
-    var url = "https://raw.githubusercontent.com/kueikuei/JSONP/master/testJson.json";
-	var request = new XMLHttpRequest();
-	request.open("GET",url);
-	request.onload = function(){
-		if(request.status==200){
-			alert("Data received!");
-		}
-	request.send(null);
+//  window.onload = function(){
+// 	// var url = "https://raw.githubusercontent.com/bethrobson/Head-First-HTML5/master/chapter6/sales.json"; 
+//     var url = "https://raw.githubusercontent.com/kueikuei/JSONP/master/testJson.json";
+// 	var request = new XMLHttpRequest();
+// 	request.open("GET",url);
+// 	request.onload = function(){
+// 		if(request.status==200){
+// 			alert("Data received!");
+// 		}
+// 	request.send(null);
+// 	}
+
+// 	//
+
+// }
+
+// window.onload=testJson;
+
+// function testJson(){
+// 	// var url = "https://raw.githubusercontent.com/bethrobson/Head-First-HTML5/master/chapter6/sales.json"; 
+//     var url = "http://gumball.wickedlysmart.com";
+// 	var request = new XMLHttpRequest();
+// 	request.open("GET",url);
+// 	request.onload = function(){
+// 		if(request.status==200){
+// 			alert("Data received!");
+// 		}
+// 	request.send(null);
+// 	}
+ 
+// 	//
+
+// }
+
+
+
+
+function testJson(data){
+	if(data){
+		alert("Data received!");
+	}else{
+		alert("Null");
 	}
 }
