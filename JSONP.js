13f8fa0a3b0de9707b6acfd18d008500504858f2
kueikuei <kueikuei@users.noreply.github.com>
2017-05-10 16:30:15 +0800

    Add files via upload

--- JSONP.js
+++ JSONP.js
@@ -1,18 +1,18 @@
-//  window.onload = function(){
-// 	// var url = "https://raw.githubusercontent.com/bethrobson/Head-First-HTML5/master/chapter6/sales.json"; 
-//     var url = "https://raw.githubusercontent.com/kueikuei/JSONP/master/testJson.json";
-// 	var request = new XMLHttpRequest();
-// 	request.open("GET",url);
-// 	request.onload = function(){
-// 		if(request.status==200){
-// 			alert("Data received!");
-// 		}
-// 	request.send(null);
-// 	}
+ window.onload = function(){
+	// var url = "https://raw.githubusercontent.com/bethrobson/Head-First-HTML5/master/chapter6/sales.json"; 
+    var url = "https://rawgit.com/kueikuei/JSONP/master/testJson.json";
+	var request = new XMLHttpRequest();
+	request.open("GET",url);
+	request.onload = function(){
+		if(request.status==200){
+			alert("Data received!");
+		}
+	request.send(null);
+	}
 
-// 	//
+	//
 
-// }
+}
 
 // window.onload=testJson;
 
@@ -35,10 +35,10 @@
 
 
 
-function testJson(data){
-	if(data){
-		alert("Data received!");
-	}else{
-		alert("Null");
-	}
-}
+// function testJson(data){
+// 	if(data){
+// 		alert("Data received!");
+// 	}else{
+// 		alert("Null");
+// 	}
+// }
